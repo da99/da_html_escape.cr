c1541d4367da40673334c231fac7bbe5707890b7
da99 <i-hate-spam-1234567@mailinator.com>
2017-09-21 22:05:09 -0500

    Improved escaping performance using a tuple instead of function calls.

--- src/escape.cr
+++ src/escape.cr
@@ -1,13 +1,23 @@
 
 module DA_HTML
 
+  REGEX_UNSAFE_CHARS = /[^\ -~\n]+|[<>'"&]+/
+
+  CHAR_HEX = {
+    60 => "<".codepoints.first.to_s(16),
+    62 => ">".codepoints.first.to_s(16),
+    39 => "'".codepoints.first.to_s(16),
+    34 => "\"".codepoints.first.to_s(16),
+    38 => "&".codepoints.first.to_s(16)
+  }
+
   def escape(source : String)
     return nil unless source.valid_encoding?
     clean(source)
-      .gsub(/[^\ -~\n]+|[<>'"&]+/){ |match|
+      .gsub(REGEX_UNSAFE_CHARS){ |match|
       # space == \u{20}=(space) , ~ == \u{7E}
         match.codepoints.map { |x|
-          "&#x#{x.to_s(16)};"
+          "&#x#{CHAR_HEX[x]? || x.to_s(16)};"
         }.join
       }
   end
