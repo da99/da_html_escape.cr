f5d37fe950ab805ec41d95127e11fcd378a0513a
da99 <i-hate-spam-1234567@mailinator.com>
2017-09-22 00:25:05 -0500

    Added: spec for 31 ASCII char during :unescape.

--- specs/unescape.cr
+++ specs/unescape.cr
@@ -1,6 +1,10 @@
 
 describe ":unescape" do
 
+  it "should replace \"field separator\" (31 ASCII) with a space" do
+    assert_escape "a ", "a\u{1f}"
+  end # === it "should replace \"field separator\" with a space"
+
   it "should decode apos entity" do
     assert_unescape "é'", "&eacute;&apos;"
   end
